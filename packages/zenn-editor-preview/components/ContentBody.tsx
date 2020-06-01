ceb20a10f6368ac62df70ca2ca99cbfd38a99e16
catnose-onair <nodding.com@gmail.com>
2020-06-01 21:08:58 +0900

    fix: sidebar layout

--- packages/zenn-editor-preview/components/ContentBody.tsx
+++ packages/zenn-editor-preview/components/ContentBody.tsx
@@ -6,7 +6,9 @@ const ContentBody: React.FC<{ content: string }> = ({ content }) => {
       <div
         className="znc"
         style={{ margin: `40px 0` }}
-        dangerouslySetInnerHTML={{ __html: content }}
+        dangerouslySetInnerHTML={{
+          __html: content || "✍️本文を入力してください",
+        }}
       />
     </ContentWrapper>
   );
