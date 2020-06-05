ea00b89d4a2cd1b22fa1c0f6a454f2e28570f6bb
catnose-onair <nodding.com@gmail.com>
2020-06-05 20:44:50 +0900

    clean: bin server

--- packages/zenn-editor-preview/bin/zenn.js
+++ packages/zenn-editor-preview/bin/zenn.js
@@ -5,9 +5,11 @@ const { createServer } = require("http");
 const { parse } = require("url");
 const next = require("next");
 
-const dev = process.env.NODE_ENV !== "production";
+// always production
+// const dev = process.env.NODE_ENV !== "production";
+const dev = false;
 
-const app = next({ dev: false, dir: srcDir });
+const app = next({ dev, dir: srcDir });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
@@ -15,15 +17,7 @@ app.prepare().then(() => {
     // Be sure to pass `true` as the second argument to `url.parse`.
     // This tells it to parse the query portion of the URL.
     const parsedUrl = parse(req.url, true);
-    const { pathname, query } = parsedUrl;
-
-    if (pathname === "/a") {
-      app.render(req, res, "/a", query);
-    } else if (pathname === "/b") {
-      app.render(req, res, "/b", query);
-    } else {
-      handle(req, res, parsedUrl);
-    }
+    handle(req, res, parsedUrl);
   }).listen(3000, (err) => {
     if (err) throw err;
     console.log("> Ready on http://localhost:3000");
