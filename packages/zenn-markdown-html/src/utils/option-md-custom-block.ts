afe3efe2a5beb04fab91f4791c0aea6a38427bd1
catnose-onair <nodding.com@gmail.com>
2020-06-10 15:25:37 +0900

    fix: content styles

--- packages/zenn-markdown-html/src/utils/option-md-custom-block.ts
+++ packages/zenn-markdown-html/src/utils/option-md-custom-block.ts
@@ -44,6 +44,6 @@ export const optionCustomBlock = {
     if (!str?.match(/^https:\/\/twitter.com\/[a-zA-Z0-9\_\-\/]+$/)) {
       return "ツイートページのURLを指定してください";
     }
-    return `<blockquote class="twitter-tweet"><a href="${str}"></a></blockquote>`;
+    return `<div class="tweet-container"><blockquote class="twitter-tweet"><a href="${str}"></a></blockquote></div>`;
   },
 };
