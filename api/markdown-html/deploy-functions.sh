063f21b32433116d3638d20ee235a39de5c50a60
catnose-onair <nodding.com@gmail.com>
2020-08-14 16:54:45 +0900

    remove: api

--- api/markdown-html/deploy-functions.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-gcloud functions deploy markdownHtml \
-  --trigger-http  \
-  --runtime=nodejs10 \
-  --source=.\
-  --region=asia-northeast1 \
-  --memory=256\
-  --timeout=90\
-  --allow-unauthenticated
