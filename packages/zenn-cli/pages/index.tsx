a5cc928c4606cd0c96b961e5c899617e3781783f
catnose-onair <nodding.com@gmail.com>
2020-08-23 11:20:24 +0900

    feat: cli home

--- packages/zenn-cli/pages/index.tsx
+++ packages/zenn-cli/pages/index.tsx
@@ -1,6 +1,7 @@
 import { NextPage, GetServerSideProps } from "next";
 import Head from "next/head";
 import { MainContainer } from "@components/MainContainer";
+import { ContentWrapper } from "@components/ContentWrapper";
 import { NavCollections } from "@types";
 import { getAllContentsNavCollections } from "@utils/nav-collections";
 
@@ -10,15 +11,40 @@ type Props = {
 
 const Page: NextPage<Props> = ({ allContentsNavCollections }) => {
   return (
-    <div>
+    <>
       <Head>
-        <title>Welcome!</title>
+        <title>Welcome to Zenn Editor!</title>
       </Head>
-
-      <MainContainer navCollections={allContentsNavCollections}>
-        Welcome!
-      </MainContainer>
-    </div>
+      <div>
+        <MainContainer navCollections={allContentsNavCollections}>
+          <ContentWrapper>
+            <article className="home">
+              <div className="home__content">
+                <h1 className="home__title">
+                  <img
+                    src="/logo.svg"
+                    alt="Zenn Editor"
+                    width={480}
+                    height={56}
+                  />
+                </h1>
+                <p className="home__description">
+                  ローカルのテキストエディターでZennのコンテンツを編集しましょう
+                </p>
+                <a
+                  href="https://zenn.dev/zenn/how-to-use-zenn-cli"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  className="home__link"
+                >
+                  使い方を知る
+                </a>
+              </div>
+            </article>
+          </ContentWrapper>
+        </MainContainer>
+      </div>
+    </>
   );
 };
 
