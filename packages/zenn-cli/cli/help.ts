71e0da0de8256a8794fda50595e8286db7baf934
catnose-onair <nodding.com@gmail.com>
2020-09-19 20:57:39 +0900

    fix: help text

--- packages/zenn-cli/cli/help.ts
+++ packages/zenn-cli/cli/help.ts
@@ -5,8 +5,8 @@ export const exec: cliCommand = () => {
   Command:
     zenn init         コンテンツ管理用のディレクトリを作成。初回のみ実行
     zenn preview      コンテンツをブラウザでプレビュー
-    zenn new:article  新しいarticleを追加
-    zenn new:book     新しいbookを追加
+    zenn new:article  新しい記事を追加
+    zenn new:book     新しい本を追加
 
     👇詳細
     https://zenn.dev/zenn/articles/zenn-cli-guide
