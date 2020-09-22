96aac1291c284c0ab2430937d6c14d55302c48f6
Noriaki Watanabe <nabeyang@gmail.com>
2020-09-22 15:14:13 +0900

    Fix duplicate command of the usage

--- packages/zenn-cli/cli/constants.ts
+++ packages/zenn-cli/cli/constants.ts
@@ -3,7 +3,6 @@ Command:
   zenn init         コンテンツ管理用のディレクトリを作成。初回のみ実行
   zenn preview      コンテンツをブラウザでプレビュー
   zenn new:article  新しい記事を追加
-  zenn new:article  新しい記事を追加
   zenn new:book     新しい本を追加
   zenn help         ヘルプ
 
