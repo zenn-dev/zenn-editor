d62797105015a8e8cf1d1f7ede9cb0a7c8acbbd7
Ry0taK <49341894+Ry0taK@users.noreply.github.com>
2020-10-02 13:16:41 +0900

    fix: change regular expression to only match emoji

--- packages/zenn-cli/utils/validator.ts
+++ packages/zenn-cli/utils/validator.ts
@@ -45,7 +45,7 @@ const validateEmojiFormat: ItemValidator = {
   isCritical: true,
   getMessage: () => "使用できない絵文字（emoji）が指定されています",
   isInvalid: (item: Article) => {
-    const emojiRegex = /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/;
+    const emojiRegex = /^(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])$/;
     return item.emoji && !item.emoji.match(emojiRegex);
   },
 };
