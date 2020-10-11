f559159d19b733c061de0ac4bf94bcfb44a7a576
j5c8k6m8 <j5c8k6m8@gmail.com>
2020-10-12 08:15:22 +0900

    デフォルトのrendererを使用するように

--- packages/zenn-markdown-html/src/utils/plugin-md-custom.ts
+++ packages/zenn-markdown-html/src/utils/plugin-md-custom.ts
@@ -1,65 +1,32 @@
-// override: https://github.com/markdown-it/markdown-it/blob/11.0.1/lib/renderer.js
-import MarkdownIt from 'markdown-it'
-import { unescapeAll, escapeHtml } from 'markdown-it/lib/common/utils'
-import Token from 'markdown-it/lib/token';
+import MarkdownIt from "markdown-it";
+import { escapeHtml } from "markdown-it/lib/common/utils";
 
-export function customRendererFence(markdownit: MarkdownIt): void {
+export function customRendererFence(md: MarkdownIt) {
+  // default renederer
+  const defaultRender =
+    md.renderer.rules.fence ||
+    function (tokens, idx, options, env, self) {
+      return self.renderToken(tokens, idx, options);
+    };
 
-    markdownit.renderer.rules.fence = function (tokens, idx, options, env, slf) {
-  var token = tokens[idx],
-      info = token.info ? unescapeAll(token.info).trim() : '',
-      langName = '',
-      fileNameTag = '',
-      langInfo, highlighted, i, tmpAttrs, tmpToken;
+  // override fence
+  md.renderer.rules.fence = function (...args) {
+    const [tokens, idx] = args;
+    // e.g. info = "js:fooBar.js"
+    const langInfo = tokens[idx].info.split(/:/);
+    const langName = langInfo?.length ? langInfo[0].trim() : ""; // e.g "js"
+    const filename = langName.length && langInfo[1] ? langInfo[1] : ""; // e.g "fooBar.js"
 
-  if (info) {
-    langInfo = info.split(/\s+/g)[0].split(/:/g);
-    langName = langInfo[0];
-    if (langInfo.length > 1 && langInfo[1]) {
-      fileNameTag = '<div class="code-file-name">' + escapeHtml(langInfo[1]) + '</div>';
-    }
-  }
+    // override info (e.g "js:fooBar.js" to "js")
+    tokens[idx].info = langName;
+    const originalHTML = defaultRender(...args);
+    if (tokens[idx].content.length === 0) return originalHTML;
 
-  if (options.highlight) {
-    highlighted = options.highlight(token.content, langName) || escapeHtml(token.content);
-  } else {
-    highlighted = escapeHtml(token.content);
-  }
-
-  if (highlighted.indexOf('<pre') === 0) {
-    if (fileNameTag) {
-      return highlighted.replace(/^<pre[^>]*>/, '$&' + fileNameTag) + '\n';
-    } else {
-      return highlighted + '\n';
-    }
-  }
-
-  // If language exists, inject class gently, without modifying original token.
-  // May be, one day we will add .clone() for token and simplify this part, but
-  // now we prefer to keep things local.
-  if (langName) {
-    i        = token.attrIndex('class');
-    tmpAttrs = token.attrs ? token.attrs.slice() : [];
-
-    if (i < 0) {
-      tmpAttrs.push([ 'class', options.langPrefix + langName ]);
-    } else {
-      tmpAttrs[i][1] += ' ' + options.langPrefix + langName;
-    }
-
-    // Fake token just to render attributes
-    tmpToken = {
-      attrs: tmpAttrs
-    } as Token;
-
-    return  '<pre>' + fileNameTag + '<code' + slf.renderAttrs(tmpToken) + '>'
-    + highlighted
-    + '</code></pre>\n';
-  }
-
-  return  '<pre>' + fileNameTag + '<code' + slf.renderAttrs(token) + '>'
-        + highlighted
-        + '</code></pre>\n';
-
+    return `
+      <div class="code-block-container">
+          <span class="code-filename">${escapeHtml(filename)}</span>
+        ${originalHTML}
+      </div>
+      `;
   };
 }
