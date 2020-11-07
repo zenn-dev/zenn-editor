0f319441da77fd42d182406e8fdffcda1dbfcb5d
catnose-onair <nodding.com@gmail.com>
2020-11-08 01:21:24 +0900

    fix: extract embedding link card method from initEmbed

---
+++ packages/zenn-init-embed/src/link-to-card.ts
@@ -0,0 +1,18 @@
+export default function linkToCard() {
+  const targetLinks = document.querySelectorAll(".znc > p > a")
+  targetLinks.forEach((linkElem)=>{
+    const prevSibling = linkElem.previousSibling;
+    const breaksBeforeLink = !prevSibling || prevSibling.textContent === "\n" // 直前の要素が存在しない or <br/>（= \n）の場合のみカード化する
+    if(!breaksBeforeLink) return;
+
+    const href = linkElem.getAttribute("href")
+    const anchorText = linkElem.innerHTML.trim()
+    const isHrefSameAsAnchor = href === anchorText
+    if(!href || !href.startsWith("http") || !isHrefSameAsAnchor) return;
+
+    const iframContainer = document.createElement("div");
+    iframContainer.classList.add('embed-zenn-link');
+    iframContainer.innerHTML = `<iframe src="https://asia-northeast1-zenn-dev-production.cloudfunctions.net/iframeLinkCard?url=${encodeURIComponent(href)}" frameborder="0" scrolling="no"></iframe>`
+    linkElem.parentElement?.insertBefore(iframContainer, linkElem);
+  })
+}
