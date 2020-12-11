231baf284f8f6fc02dd0c4a127726811654e1633
catnose-onair <nodding.com@gmail.com>
2020-12-11 22:23:56 +0900

    fix: markdown html error

---
+++ packages/zenn-markdown-html/src/utils/md-default.ts
@@ -0,0 +1,24 @@
+import MarkdownIt from 'markdown-it';
+
+import { mdRendererFence } from './md-renderer-fence';
+import { mdLinkifyClass } from './md-linkify-class';
+import markdownItPrism from './prism';
+const mdTexMath = require('@catnose99/markdown-it-texmath');
+
+export function defaultPlugin(md: MarkdownIt) {
+  md.linkify.set({ fuzzyLink: false });
+  md.use(markdownItPrism)
+    .use(mdRendererFence)
+    .use(mdTexMath, {
+      engine: require('katex'),
+      delimiters: 'dollars',
+      katexOptions: { macros: { '\\RR': '\\mathbb{R}' } },
+    })
+    .use(require('markdown-it-link-attributes'), {
+      pattern: /^(?!https:\/\/zenn\.dev\/)/,
+      attrs: {
+        rel: 'nofollow',
+      },
+    })
+    .use(mdLinkifyClass);
+}
