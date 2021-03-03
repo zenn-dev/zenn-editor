c3a68cafecea04456a80080a6043a43f03b0b985
Kyoichiro Yamada <me@kyoh86.dev>
2021-03-04 00:22:40 +0900

    Revert "Merge pull request #126 from zenn-dev/revert-125-list"
    This reverts commit be39a3d16665412f8ba0be96225c87a2fd5c649b, reversing
    changes made to 07306f08e64ea53a3e081cfc91932de3bfdab4be.

---
+++ packages/zenn-cli/cli/list-books.ts
@@ -0,0 +1,77 @@
+import path from 'path';
+import fs from 'fs';
+import matter from 'gray-matter';
+import arg from 'arg';
+import { Book } from '../types';
+import { cliCommand } from '.';
+import colors from 'colors/safe';
+import { invalidOption, listBooksHelpText } from './constants';
+
+function parseArgs(argv: string[] | undefined) {
+  try {
+    return arg(
+      {
+        // Types
+        '--format': String,
+        '--help': Boolean,
+        // Alias
+        '-h': '--help',
+      },
+      { argv }
+    );
+  } catch (e) {
+    if (e.code === 'ARG_UNKNOWN_OPTION') {
+      console.log(colors.red(invalidOption));
+    } else {
+      console.log(colors.red('エラーが発生しました'));
+    }
+    console.log(listBooksHelpText);
+    return null;
+  }
+}
+
+const bookFormatters: { [key: string]: (book: Book) => string } = {
+  tsv: (book: Book) => book.slug + (!!book.title ? '\t' + book.title : ''),
+  json: (book: Book) => JSON.stringify(book),
+};
+
+export const exec: cliCommand = (argv) => {
+  const args = parseArgs(argv);
+  if (!args) return;
+
+  if (args['--help']) {
+    console.log(listBooksHelpText);
+    return;
+  }
+
+  const format = args['--format'] || 'tsv';
+  if (!(format in bookFormatters)) {
+    console.error(
+      `エラー：formatの値（${format}）が不正です。"tsv" または "json" を指定してください`
+    );
+    process.exit(1);
+  }
+  const formatter = bookFormatters[format];
+
+  const dir = path.join(process.cwd(), 'books');
+  fs.readdirSync(dir, {
+    encoding: 'utf-8',
+    withFileTypes: true,
+  })
+    .filter((dirent) => dirent.isFile() && /\.md$/.test(dirent.name))
+    .map(({ name }) => {
+      const slug = name.replace(/\.md$/, '');
+      let book: Book = {
+        slug,
+      };
+      try {
+        const fileRaw = fs.readFileSync(path.join(dir, name), 'utf8');
+        const { data } = matter(fileRaw);
+        book = { ...book, ...data };
+      } catch {}
+      return book;
+    })
+    .forEach(book => {
+      console.log(formatter(book));
+    });
+};
