f4a1d9e3fc885f9e36b8e2ce26ef13f7617a6e83
catnose99 <nodding.com@gmail.com>
2021-07-05 17:57:45 +0900

    feat: setttings component

--- packages/zenn-cli/src/client/components/sidebar/Settings.tsx
+++ packages/zenn-cli/src/client/components/sidebar/Settings.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import CheckRoundedIcon from '@material-ui/icons/CheckRounded';
 
 type LabelValue = string;
 
@@ -14,10 +15,20 @@ type Props<T extends LabelValue> = {
   setValue: (value: T) => void;
   position: 'left' | 'center' | 'right';
   openButtonIcon: React.ReactNode;
+  openButtonAriaLabel: string;
+  width: number;
 };
 
 export const Settings = <T extends LabelValue>(props: Props<T>) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  function handleItemClick(value: T) {
+    props.setValue(value);
+    // improve ux
+    setTimeout(() => {
+      setIsOpen(false);
+    }, 200);
+  }
 
   return (
     <StyledSettings className="settings">
@@ -25,32 +36,99 @@ export const Settings = <T extends LabelValue>(props: Props<T>) => {
         {props.openButtonIcon}
       </button>
       {isOpen && (
-        <div className="settings__popover-container">
-          <button
+        <>
+          <div
             className="settings__backdrop"
             onClick={() => setIsOpen(false)}
           />
-          <div className="settings__popover">
+          <div
+            className={`settings__popover settings__popover--${props.position}`}
+            style={{ width: props.width }}
+          >
             {props.options.map(({ value, label }) => (
               <button
                 key={`option-${value}`}
                 value={value}
-                onClick={() => props.setValue(value)}
-                className={`settings__option ${
-                  value === props.value ? 'settings__option--active' : ''
-                }`}
+                onClick={() => handleItemClick(value)}
+                className={`settings__option`}
+                aria-selected={value === props.value}
               >
+                <CheckRoundedIcon className="settings__option-icon" />
                 {label}
               </button>
             ))}
           </div>
-        </div>
+        </>
       )}
     </StyledSettings>
   );
 };
 
 const StyledSettings = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+
+  .settings__open {
+    display: inline-flex;
+    align-items: center;
+    color: var(--c-gray);
+    &:hover {
+      color: var(--c-body);
+    }
+  }
   .settings__backdrop {
+    position: fixed;
+    background: transparent;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+  }
+  .settings__backdrop,
+  .settings__popover {
+    z-index: 9999;
+  }
+  .settings__popover {
+    position: absolute;
+    background: #fff;
+    top: calc(100% + 8px);
+    box-shadow: 0 2px 8px rgb(15 26 62 / 35%);
+    border-radius: 5px;
+    font-size: 13.5px;
+    overflow: hidden;
+    padding: 5px;
+  }
+  .settings__popover--left {
+    left: -10px;
+  }
+  .settings__popover--right {
+    right: -10px;
+  }
+  .settings__popover--center {
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  .settings__option {
+    width: 100%;
+    text-align: left;
+    padding: 8px 8px 8px 22px;
+    border-radius: 4px;
+    &:hover {
+      background: var(--c-gray-bg);
+    }
+    &[aria-selected='true'] {
+      .settings__option-icon {
+        opacity: 1;
+      }
+    }
+  }
+  .settings__option-icon {
+    position: absolute;
+    left: 8px;
+    width: 16px;
+    height: 16px;
+    opacity: 0;
+    color: var(--c-primary);
   }
 `;
