b5fbb0d1bbed80fe0ad4d95e2b3405ea5b156fbe
cm-igarashi-ryosuke <igarashi.ryosuke@classmethod.jp>
2021-07-27 10:11:03 +0900

    fix: enable settings without errors.

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,16 +12,16 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended',
+    // 'plugin:react/recommended', // ESLint couldn't find the plugin "eslint-plugin-react".
+    // 'plugin:jsx-a11y/recommended', // ESLint couldn't find the plugin "eslint-plugin-jsx-a11y".
     // Prettier plugin and recommended rules
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
+    // 'prettier/@typescript-eslint', // "prettier/@typescript-eslint" has been merged into "prettier" in eslint-config-prettier 8.0.0.
+    // 'plugin:prettier/recommended', // ESLint couldn't find the plugin "eslint-plugin-prettier".
   ],
   rules: {
     // Include .prettierrc.js rules
     'no-empty': 'off',
-    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    // 'prettier/prettier': ['error', {}, {usePrettierrc: true}], // Definition for rule 'prettier/prettier' was not found
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
@@ -29,19 +29,19 @@ module.exports = {
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    'jsx-a11y/alt-text': 'off',
-    'jsx-a11y/accessible-emoji': 'off',
-    'jsx-a11y/anchor-is-valid': 'off',
-    'jsx-a11y/label-has-associated-control': [
-      'error',
-      {
-        labelComponents: [],
-        labelAttributes: [],
-        controlComponents: [],
-        assert: 'either',
-        depth: 25,
-      },
-    ],
+    // 'jsx-a11y/alt-text': 'off', // Definition for rule 'jsx-a11y/label-has-associated-control' was not found
+    // 'jsx-a11y/accessible-emoji': 'off',
+    // 'jsx-a11y/anchor-is-valid': 'off',
+    // 'jsx-a11y/label-has-associated-control': [
+    //   'error',
+    //   {
+    //     labelComponents: [],
+    //     labelAttributes: [],
+    //     controlComponents: [],
+    //     assert: 'either',
+    //     depth: 25,
+    //   },
+    // ],
   },
   settings: {
     react: {
