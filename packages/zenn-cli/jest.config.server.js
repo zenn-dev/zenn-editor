1c9f0e9562f916c65fdbe3644a0e413db5575fd9
uttk-dev <97154037+uttk-dev@users.noreply.github.com>
2022-01-21 06:46:44 +0900

    feat: @swc/jestを使ってテストを高速化した

--- packages/zenn-cli/jest.config.server.js
+++ packages/zenn-cli/jest.config.server.js
@@ -1,15 +1,9 @@
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
   moduleFileExtensions: ['js', 'json', 'ts'],
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.ts$': '@swc/jest',
   },
   testMatch: ['<rootDir>/src/server/__tests__/**/*.test.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.server.json',
-    },
-  },
   resetMocks: true,
 };
