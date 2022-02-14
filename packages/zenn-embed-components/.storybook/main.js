70da758da569f359b92fe15db8ac58f72bcb32e6
uttk-dev <97154037+uttk-dev@users.noreply.github.com>
2022-02-14 17:33:09 +0900

    feat: emotionを使えるように環境構築した

--- packages/zenn-embed-components/.storybook/main.js
+++ packages/zenn-embed-components/.storybook/main.js
@@ -1,11 +1,9 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "framework": "@storybook/react"
-}
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  framework: '@storybook/react',
+  babel: async (options) => ({
+    ...options,
+    presets: [...options.presets, '@emotion/babel-preset-css-prop'],
+  }),
+};
