ff7c8dc707631f3fa363944efde2a8a77a060aba
uttk-dev <97154037+uttk-dev@users.noreply.github.com>
2022-02-15 18:36:36 +0900

    feat: リンクカードのコンポーネントを実装した

---
+++ packages/zenn-embed-components/src/components/link-card/EmbedBaseLinkCard.tsx
@@ -0,0 +1,164 @@
+import { css } from '@emotion/react';
+import { useState } from 'react';
+import { GithubIcon } from '../Icons/GithubIcon';
+import { LinkData } from './types';
+
+export interface EmbedBaseLinkCardProps extends LinkData {}
+
+export const EmbedBaseLinkCard = ({
+  url,
+  title,
+  imageUrl,
+  hostname,
+  urlOrigin,
+  description,
+  shouldNofollow,
+}: EmbedBaseLinkCardProps) => {
+  const [_imageUrl, setImageUrl] = useState(imageUrl);
+
+  const hideImage = () => {
+    setImageUrl(undefined);
+  };
+
+  return (
+    <a
+      href={url}
+      rel={
+        shouldNofollow ? 'nofollow noreferrer noopener' : 'noreferrer noopener'
+      }
+      target="_blank"
+      css={styles}
+      className="link"
+    >
+      <div className="main">
+        <h1
+          className={
+            title && title.length < 10 ? 'title title--large' : 'title'
+          }
+        >
+          {title || url}
+        </h1>
+
+        {description && <div className="description">{description}</div>}
+
+        {hostname && (
+          <div className="meta">
+            {hostname === 'github.com' ? (
+              /* workaround: github favicon was not available */
+              <GithubIcon />
+            ) : (
+              <img
+                src={`https://www.google.com/s2/favicons?sz=32&domain_url=${
+                  urlOrigin ?? ''
+                }`}
+                width="14"
+                height="14"
+                className="favicon"
+                alt=""
+              />
+            )}
+            {hostname}
+          </div>
+        )}
+      </div>
+
+      {_imageUrl && (
+        <div className="thumbnail">
+          <img
+            src={_imageUrl}
+            alt=""
+            className="thumbnail-img"
+            onError={hideImage}
+          />
+        </div>
+      )}
+    </a>
+  );
+};
+
+const styles = css`
+  & {
+    display: flex;
+    align-items: center;
+    font-size: 16.5px;
+    height: 120px;
+    line-height: 1.5;
+    transition: 0.2s;
+    color: rgba(0, 0, 0, 0.82);
+    text-decoration: none;
+    background: #fff;
+
+    &:hover {
+      background: rgba(239, 246, 251, 0.7);
+    }
+  }
+  .thumbnail {
+    height: 120px;
+    max-width: 230px; /* Allow aspect ratio as wide as Twitter Card */
+  }
+  .main {
+    flex: 1;
+    padding: 0.8em 1.2em;
+    min-width: 0; /* Workaround to enable text overflow ellipsis in flex container. ref: https://codepen.io/distums/pen/VaNjBo */
+  }
+  .title,
+  .description {
+    overflow: hidden;
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+  }
+  .title {
+    margin: 0;
+    font-size: 1em;
+    -webkit-line-clamp: 2;
+    max-height: 3.05em;
+    user-select: none;
+    word-break: break-word;
+  }
+  .title--large {
+    font-size: 1.08em;
+  }
+  .description {
+    margin-top: 0.5em;
+    color: #77838c;
+    font-size: 0.8em;
+    -webkit-line-clamp: 1;
+    max-height: 1.55em;
+  }
+
+  .meta {
+    margin-top: 0.5em;
+    font-size: 0.78em;
+    display: flex;
+    align-items: center;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+  .favicon {
+    margin-right: 6px;
+    flex-shrink: 0;
+  }
+
+  .thumbnail-img {
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+    flex-shrink: 0;
+  }
+
+  @media only screen and (max-width: 480px) {
+    & {
+      font-size: 15px;
+    }
+    .meta {
+      margin-top: 0.4em;
+    }
+    .main {
+      padding: 0.7em 1em;
+    }
+    .thumbnail {
+      width: 120px;
+    }
+  }
+`;
---
+++ packages/zenn-embed-components/src/components/link-card/EmbedGithubLinkCard.tsx
@@ -0,0 +1,157 @@
+import { css } from '@emotion/react';
+import { GithubCodeIcon } from '../Icons/GithubCodeIcon';
+import { GithubForkIcon } from '../Icons/GithubForkIcon';
+import { GithubIcon } from '../Icons/GithubIcon';
+import { GithubStarIcon } from '../Icons/GithubStarIcon';
+import { GithubRepoData } from './types';
+
+export interface EmbedGithubLinkCardProps extends GithubRepoData {}
+
+export const EmbedGithubLinkCard = ({
+  url,
+  name,
+  owner,
+  language,
+  forksCount,
+  description,
+  stargazersCount,
+}: EmbedGithubLinkCardProps) => {
+  return (
+    <a
+      href={url}
+      rel="noreferrer noopener"
+      target="_blank"
+      css={styles}
+      className="link"
+    >
+      <div className="main">
+        <div className="title">
+          <div className="github-icon">
+            <GithubIcon />
+          </div>
+
+          <h1 className="name">
+            {owner}
+            <span className="slash">/</span>
+            {name}
+          </h1>
+        </div>
+
+        {description && <div className="description">{description}</div>}
+
+        <div className="meta-container">
+          {stargazersCount && (
+            <span className="meta">
+              <div className="github-icon star">
+                <GithubStarIcon />
+              </div>
+              {stargazersCount}
+            </span>
+          )}
+
+          {forksCount && (
+            <span className="meta">
+              <div className="github-icon fork">
+                <GithubForkIcon />
+              </div>
+              {forksCount}
+            </span>
+          )}
+
+          {language && (
+            <span className="meta">
+              <div className="github-icon code">
+                <GithubCodeIcon />
+              </div>
+              {language}
+            </span>
+          )}
+        </div>
+      </div>
+    </a>
+  );
+};
+
+const styles = css`
+  & {
+    display: flex;
+    align-items: center;
+    font-size: 16px;
+    height: 120px;
+    line-height: 1.4;
+    transition: 0.2s;
+    color: #7b8288;
+    text-decoration: none;
+    background: #fff;
+    padding: 0 1rem;
+
+    &:hover {
+      background: rgba(239, 246, 251, 0.7);
+    }
+  }
+  .title {
+    display: flex;
+    align-items: center;
+  }
+  .github-icon {
+    width: 22px;
+    height: 22px;
+    flex-shrink: 0;
+    margin-right: 6px;
+    color: #191919;
+
+    &.star {
+      width: 16px;
+      height: 16px;
+    }
+    &.fork {
+      width: 15px;
+      height: 15px;
+    }
+    &.code {
+      width: 15px;
+      height: 15px;
+    }
+  }
+  .name {
+    margin: 0;
+    word-break: break-all;
+    font-size: 1.15em;
+    color: #0366d6;
+    line-height: 1.2;
+  }
+  .slash {
+    color: #7b8288;
+    margin: 0 4px;
+    font-weight: normal;
+  }
+
+  .description {
+    margin-top: 10px;
+    font-size: 0.85em;
+    display: -webkit-box;
+    -webkit-line-clamp: 1;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+  }
+  .meta-container {
+    margin-top: 10px;
+    display: flex;
+    align-items: center;
+  }
+  .meta {
+    margin-right: 15px;
+    font-size: 13px;
+    display: inline-flex;
+    align-items: center;
+  }
+  .meta-icon {
+    margin-right: 5px;
+  }
+
+  @media only screen and (max-width: 480px) {
+    .name {
+      font-size: 1.05em;
+    }
+  }
+`;
