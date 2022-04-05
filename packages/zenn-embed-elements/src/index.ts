246e9d48a245f570ee59304f9f74bed7f9b7e505
uttk-dev <97154037+uttk-dev@users.noreply.github.com>
2022-04-06 07:35:39 +0900

    fix: zenn-embed-elementsのビルドエラーを修正

--- packages/zenn-embed-elements/src/index.ts
+++ packages/zenn-embed-elements/src/index.ts
@@ -1,9 +1,3 @@
-import { EmbedGist } from './classes/gist';
-import { EmbedTweet } from './classes/tweet';
 import { EmbedKatex } from './classes/katex';
-import { EmbedMermaid } from './classes/mermaid';
 
-customElements.define('embed-gist', EmbedGist);
-customElements.define('embed-tweet', EmbedTweet);
 customElements.define('embed-katex', EmbedKatex);
-customElements.define('embed-mermaid', EmbedMermaid);
