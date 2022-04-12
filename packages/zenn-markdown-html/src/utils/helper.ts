90cdef232a2e7d703dab723f7e0548b7cf3436ee
catnose99 <nodding.com@gmail.com>
2022-04-12 12:03:03 +0900

    余白の微調整

--- packages/zenn-markdown-html/src/utils/helper.ts
+++ packages/zenn-markdown-html/src/utils/helper.ts
@@ -41,9 +41,11 @@ export function isValidHttpUrl(str: string) {
 type ZennEmbedTypes = 'tweet' | 'link-card' | 'mermaid' | 'github' | 'gist';
 
 export function generateEmbedIframe(type: ZennEmbedTypes, src: string): string {
+  // ユーザーからの入力値が引数として渡されたときのために念のためencodeする
+  const encodedType = encodeURIComponent(type);
+  const encodedSrc = encodeURIComponent(src);
   const id = `zenn-embedded__${Math.random().toString(16).slice(2)}`;
-  const iframeSrc = `https://embed.zenn.studio/${type}#${id}`;
-  const content = encodeURIComponent(src);
+  const iframeSrc = `https://embed.zenn.studio/${encodedType}#${id}`;
 
-  return `<div class="zenn-embedded"><iframe id="${id}" src="${iframeSrc}" data-content="${content}" frameborder="0" scrolling="no" loading="lazy"></iframe></div>`;
+  return `<div class="zenn-embedded zenn-embedded-${encodedType}"><iframe id="${id}" src="${iframeSrc}" data-content="${encodedSrc}" frameborder="0" scrolling="no" loading="lazy"></iframe></div>`;
 }
