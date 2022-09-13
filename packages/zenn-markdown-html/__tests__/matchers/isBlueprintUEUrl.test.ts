2158509fb74fd434414bb9618f2ce06d8b4c986c
uttk-dev <97154037+uttk-dev@users.noreply.github.com>
2022-09-13 18:40:20 +0900

    fix: isBlueprintUEUrlのテストを追加

---
+++ packages/zenn-markdown-html/__tests__/matchers/isBlueprintUEUrl.test.ts
@@ -0,0 +1,18 @@
+import { isBlueprintUEUrl } from '../../src/utils/url-matcher';
+
+describe('isBlueprintUEUrlのテスト', () => {
+  describe('Trueを返す場合', () => {
+    test('blueprintUEの埋め込みURL', () => {
+      const url = 'https://blueprintue.com/render/xmdvzpam';
+      expect(isBlueprintUEUrl(url)).toBe(true);
+      expect(isBlueprintUEUrl(`${url}/`)).toBe(true);
+    });
+  });
+
+  describe('Falseを返す場合', () => {
+    test('XSSを含んでいる', () => {
+      const url = `https://blueprintue.com/render/xmdvzpam/"></iframe><img src onerror=alert(document.domain)>/`;
+      expect(isBlueprintUEUrl(url)).toBe(false);
+    });
+  });
+});
