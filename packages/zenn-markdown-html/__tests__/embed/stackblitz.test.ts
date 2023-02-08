23539dfd259f80a76d1d5363aa68c036dfaeedc5
uttk-dev <97154037+uttk-dev@users.noreply.github.com>
2023-02-08 16:19:20 +0900

    feat: Stackblitz埋め込みのテストを追加

---
+++ packages/zenn-markdown-html/__tests__/embed/stackblitz.test.ts
@@ -0,0 +1,38 @@
+import { parse } from 'node-html-parser';
+import markdownToHtml from '../../src/index';
+
+describe('Stackblitz埋め込み要素のテスト', () => {
+  const validUrl = 'https://stackblitz.com/edit/test-examples';
+  const invalidUrl = '@https://bad-url.stackblitz.com/edit/test-examples';
+
+  describe('デフォルトの挙動の場合', () => {
+    test('@[stackblitz](...)を<iframe />に変換する', () => {
+      const html = markdownToHtml(`@[stackblitz](${validUrl})`);
+      const iframe = parse(html).querySelector(`span.embed-stackblitz iframe`);
+
+      expect(iframe?.attributes).toEqual(
+        expect.objectContaining({ src: validUrl })
+      );
+    });
+
+    describe('無効なURLの場合', () => {
+      test('エラーメッセージを出力する', () => {
+        const html = markdownToHtml(`@[stackblitz](${invalidUrl})`);
+        expect(html).toContain('StackBlitzのembed用のURLを指定してください');
+      });
+    });
+  });
+
+  describe('`customEmbed.stackblitz()`を設定している場合', () => {
+    test('渡した関数を実行する', () => {
+      const customizeText = 'customized text!';
+      const mock = jest.fn().mockReturnValue(customizeText);
+      const html = markdownToHtml(`@[stackblitz](${validUrl})`, {
+        customEmbed: { stackblitz: mock },
+      });
+
+      expect(mock).toBeCalled();
+      expect(html).toContain(customizeText);
+    });
+  });
+});
