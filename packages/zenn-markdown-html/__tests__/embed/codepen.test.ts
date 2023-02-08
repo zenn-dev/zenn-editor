0fecc9bfc14ddd7dd4c09f0b8a85777ece348d03
uttk-dev <97154037+uttk-dev@users.noreply.github.com>
2023-02-08 14:55:45 +0900

    feat: codepenのテストを追加

---
+++ packages/zenn-markdown-html/__tests__/embed/codepen.test.ts
@@ -0,0 +1,41 @@
+import { parse } from 'node-html-parser';
+import markdownToHtml from '../../src/index';
+
+describe('Codepen埋め込み要素のテスト', () => {
+  const validUrl = 'https://codepen.io/examples/pen/test?default-tab=hoge';
+  const invalidUrl =
+    'https://bad-example.codepen.io/examples/pen/test?default-tab=hoge';
+
+  describe('デフォルトの挙動の場合', () => {
+    test('@[codepen](...)を<iframe />に変換する', () => {
+      const html = markdownToHtml(`@[codepen](${validUrl})`);
+      const iframe = parse(html).querySelector(`span.embed-codepen iframe`);
+      const passedUrl = validUrl.replace('/pen/', '/embed/');
+
+      console.log({ html });
+      expect(iframe?.attributes).toEqual(
+        expect.objectContaining({ src: expect.stringContaining(passedUrl) })
+      );
+    });
+
+    describe('無効なURLの場合', () => {
+      test('エラーメッセージを出力する', () => {
+        const html = markdownToHtml(`@[codepen](${invalidUrl})`);
+        expect(html).toContain('CodePenのURLが不正です');
+      });
+    });
+  });
+
+  describe('`customEmbed.codepen()`を設定している場合', () => {
+    test('渡した関数を実行する', () => {
+      const customizeText = 'customized text';
+      const mock = jest.fn().mockReturnValue(customizeText);
+      const html = markdownToHtml(`@[codepen](${invalidUrl})`, {
+        customEmbed: { codepen: mock },
+      });
+
+      expect(mock).toBeCalled();
+      expect(html.includes(customizeText)).toBe(true);
+    });
+  });
+});
