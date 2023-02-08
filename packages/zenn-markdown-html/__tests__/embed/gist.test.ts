3131a769be3863b8cc353b7fc181c9902403c30d
uttk-dev <97154037+uttk-dev@users.noreply.github.com>
2023-02-08 15:16:41 +0900

    feat: gist のテストを追加

---
+++ packages/zenn-markdown-html/__tests__/embed/gist.test.ts
@@ -0,0 +1,58 @@
+import { parse } from 'node-html-parser';
+import markdownToHtml from '../../src/index';
+
+describe('GitHub Gist埋め込み要素のテスト', () => {
+  const validUrl = 'https://gist.github.com/examples/9164408';
+  const invalidUrl = 'https://bad-example.gist.github.com/examples/9164408';
+
+  describe('デフォルトの挙動の場合', () => {
+    test('@[gist](...)をリンクに変換する', () => {
+      const html = markdownToHtml(`@[gist](${validUrl})`);
+      const link = parse(html).querySelector(`a`);
+
+      expect(link?.attributes).toEqual(
+        expect.objectContaining({
+          href: validUrl,
+          rel: 'noreferrer noopener nofollow',
+          target: '_blank',
+        })
+      );
+    });
+
+    describe('無効なURLの場合', () => {
+      test('エラーメッセージを出力する', () => {
+        const html = markdownToHtml(`@[gist](${invalidUrl})`);
+        expect(html).toContain('GitHub GistのページURLを指定してください');
+      });
+    });
+
+    describe('embedOriginを設定している場合', () => {
+      test('渡した`embedOrigin`を`src`として<iframe />を表示する', () => {
+        const embedOrigin = 'https://embed.example.com';
+        const html = markdownToHtml(validUrl, { embedOrigin });
+        const iframe = parse(html).querySelector('span.zenn-embedded iframe');
+
+        expect(iframe?.attributes).toEqual(
+          expect.objectContaining({
+            src: expect.stringMatching(embedOrigin),
+            'data-content': encodeURIComponent(validUrl),
+          })
+        );
+      });
+    });
+  });
+
+  describe('customEmbed.gist()を設定している場合', () => {
+    test('渡した関数を実行する', () => {
+      const url = 'https://example.com';
+      const customizeText = 'customized text';
+      const mock = jest.fn().mockReturnValue(customizeText);
+      const html = markdownToHtml(`@[gist](${url})`, {
+        customEmbed: { gist: mock },
+      });
+
+      expect(mock).toBeCalled();
+      expect(html.includes(customizeText)).toBe(true);
+    });
+  });
+});
