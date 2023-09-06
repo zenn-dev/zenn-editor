3afa23191e3aa31500135632ff08a911a7e6d3d7
dyoshikawa <yoshikawa.daio@classmethod.jp>
2023-09-06 17:01:06 +0900

    hover時にボタンを表示する

--- packages/zenn-cli/src/client/components/InsertAnchorButtonToHeaings.tsx
+++ packages/zenn-cli/src/client/components/InsertAnchorButtonToHeaings.tsx
@@ -11,13 +11,14 @@ export const InsertAnchorButtonToHeadings: React.FC<Props> = ({ children }) => {
   const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    // コードブロックの要素を取得する
+    // 見出しの要素を取得する
     const containerElem = containerRef.current;
     if (!containerElem) return;
     const headerAnchorLinks = containerElem.querySelectorAll(
       '.header-anchor-link'
     );
-    console.log(headerAnchorLinks);
+
+    // 見出しの隣にアンカー 🔗 を表示する
     headerAnchorLinks.forEach((headerAnchorLink) => {
       const anchorButton = document.createElement('button');
       const parent = headerAnchorLink.parentElement;
@@ -27,20 +28,26 @@ export const InsertAnchorButtonToHeadings: React.FC<Props> = ({ children }) => {
       anchorButton.setAttribute(
         'style',
         `
-        position: absolute;
-        top: 0.23em;
-        left: -25px;
-        display: block;
-        width: 24px;
-        height: 1em;
-        padding-right: 5px;
+          position: absolute;
+          top: 0.23em;
+          left: -25px;
+          display: block;
+          width: 24px;
+          height: 1em;
+          padding-right: 5px;
 
-        background: url('https://zenn.dev/permanent/link-gray.svg') no-repeat
-          center;
-        background-size: 20px 20px;
-        opacity: 1;
-      `
+          background: url('https://zenn.dev/permanent/link-gray.svg') no-repeat
+            center;
+          background-size: 20px 20px;
+          opacity: 0;
+        `
       );
+
+      // hover 時にボタンを表示する
+      anchorButton.setAttribute('onMouseOver', 'this.style.opacity = 1;');
+      anchorButton.setAttribute('onMouseOut', 'this.style.opacity = 0;');
+
+      // 見出しの隣にボタンを追加する
       parent.insertBefore(anchorButton, headerAnchorLink);
 
       // tooltip のオプションを設定
@@ -53,8 +60,8 @@ export const InsertAnchorButtonToHeadings: React.FC<Props> = ({ children }) => {
       // ボタンをクリックしたら見出しのアンカーリンクをクリップボードにコピーする
       // 親要素が React で管理されてないので、アンマウント時の removeEventListener はあまり意味がないので実装しない
       anchorButton.addEventListener('click', async () => {
+        // アンカーリンクの href を取得し、クリップボードにコピーする
         const text = headerAnchorLink.getAttribute('href') ?? '';
-
         await navigator.clipboard.writeText(text);
 
         // この時にツールチップを表示する
