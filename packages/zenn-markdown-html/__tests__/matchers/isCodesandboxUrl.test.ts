fb046359b5f1d83655fde30f85374142d576ff6c
Ryosuke Igarashi <igarashi.ryosuke@classmethod.jp>
2024-02-05 17:18:41 +0900

    fix: Codesandbox URL に `+` を許容する

---
+++ packages/zenn-markdown-html/__tests__/matchers/isCodesandboxUrl.test.ts
@@ -0,0 +1,20 @@
+import { describe, test, expect } from 'vitest';
+import { isCodesandboxUrl } from '../../src/utils/url-matcher';
+
+describe('isCodesandboxUrlのテスト', () => {
+  describe('Trueを返す場合', () => {
+    test('Codesandboxの埋め込みURL', () => {
+      const url =
+        'https://codesandbox.io/embed/new?view=Editor+%2B+Preview';
+
+      expect(isCodesandboxUrl(url)).toBe(true);
+    });
+  });
+
+  describe('Falseを返す場合', () => {
+    test('XSSを含んでいる', () => {
+      const url = `https://codesandbox.io/embed/new?view=Editor+%2B+Preview"></iframe><img src onerror=alert(document.domain)>/`;
+      expect(isCodesandboxUrl(url)).toBe(false);
+    });
+  });
+});
