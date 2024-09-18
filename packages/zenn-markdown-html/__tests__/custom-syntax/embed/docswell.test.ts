32a4876c30cfda52baa8a8c94710fa9c6319c440
cm-dyoshikawa <yoshikawa.daio@classmethod.jp>
2024-09-18 09:45:54 +0900

    テスト用のURL修正

--- packages/zenn-markdown-html/__tests__/custom-syntax/embed/docswell.test.ts
+++ packages/zenn-markdown-html/__tests__/custom-syntax/embed/docswell.test.ts
@@ -16,7 +16,7 @@ describe('Docswell', () => {
   describe('DocswellのスライドURLの場合', () => {
     test('Docswellのiframeを返すこと', () => {
       const html = markdownToHtml(
-        '@[docswell](https://www.docswell.com/slide/LK7J5V/embed)'
+        '@[docswell](https://www.docswell.com/s/ku-suke/LK7J5V-hello-docswell)'
       );
       expect(html).toContain(
         '<span class="embed-block embed-docswell"><iframe src="https://www.docswell.com/slide/LK7J5V/embed" allowfullscreen="true" width="620" style="border:1px solid #ccc;display:block;margin:0px auto;padding:0px;aspect-ratio:620/349"></iframe></span>'
