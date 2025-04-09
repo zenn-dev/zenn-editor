79a5ae08a5720c72417b3e53e241f15c06e8214c
0Delta <0deltast@gmail.com>
2025-04-09 11:18:50 +0900

    fix: リンクが切れているsvgの参照をやめて直書きにする

--- packages/zenn-cli/src/client/components/ErrorMessage.tsx
+++ packages/zenn-cli/src/client/components/ErrorMessage.tsx
@@ -6,16 +6,20 @@ export const ErrorMessage: React.FC<Props> = (props) => {
   return (
     <StyledErrorMessage className="error-message">
       <div className="error-message__inner">
-        <img
-          src="https://twemoji.maxcdn.com/2/svg/1f63f.svg"
-          alt=""
-          width={90}
-          height={90}
+        <svg stroke="currentColor"
+          fill="currentColor"
           className="error-message__icon"
-        />
-        <div className="error-message__text">
-          {props.message || 'エラーが発生しました'}
-        </div>
+          stroke-width="0"
+          viewBox="0 0 24 24"
+          height={90}
+          width={90}
+          xmlns="http://www.w3.org/2000/svg">
+          <path fill="none" d="M0 0h24v24H0z"></path>
+          <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"></path>
+        </svg>
+      </div>
+      <div className="error-message__text">
+        {props.message || 'エラーが発生しました'}
       </div>
     </StyledErrorMessage>
   );
@@ -24,6 +28,7 @@ export const ErrorMessage: React.FC<Props> = (props) => {
 const StyledErrorMessage = styled.div`
   height: 100vh;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   .error-message__inner {
