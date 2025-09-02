cb767fede072f1943fc07aa359bb767b86cf744f
karintou8710 <karintou8710@gmail.com>
2025-09-03 02:39:29 +0900

    fix: convertHTMLtoEditableContent

--- packages/zenn-wysiwyg-editor/src/lib/from-markdown.ts
+++ packages/zenn-wysiwyg-editor/src/lib/from-markdown.ts
@@ -1,11 +1,17 @@
-import markdownToHtml from "zenn-markdown-html";
-import { EMBED_ORIGIN } from "./constants";
+import markdownToHtml from 'zenn-markdown-html';
+import { EMBED_ORIGIN } from './constants';
 
 export function fromMarkdown(text: string) {
   const html = markdownToHtml(text, {
     embedOrigin: EMBED_ORIGIN,
   });
-  const dom = document.createElement("div");
+
+  return convertHTMLtoEditableContent(html);
+}
+
+// 装飾付きの記事HTMLを編集可能コンテンツに変換
+export function convertHTMLtoEditableContent(html: string) {
+  const dom = document.createElement('div');
   dom.innerHTML = html;
 
   removeMessageSymbol(dom);
@@ -19,7 +25,7 @@ export function fromMarkdown(text: string) {
 }
 
 function adjustDiffCodeBlock(dom: HTMLElement) {
-  const diffCodes = dom.querySelectorAll("code.diff-highlight");
+  const diffCodes = dom.querySelectorAll('code.diff-highlight');
 
   diffCodes.forEach((code) => {
     // NodeListを配列に変換してから処理
@@ -28,24 +34,24 @@ function adjustDiffCodeBlock(dom: HTMLElement) {
     children.forEach((child) => {
       if (child.nodeType === Node.TEXT_NODE) {
         // トップレベルのテキストノードは行なのでspanで囲む
-        const span = document.createElement("span");
+        const span = document.createElement('span');
         span.appendChild(child.cloneNode());
 
         child.parentElement?.replaceChild(span, child);
       } else if (child instanceof HTMLElement) {
         // spanの装飾はDecorationでするため削除。削除しないとパースでエラーになる
         // 繋がった挿入・削除ブロックは改行コードを含むので分割する
-        if (child.textContent?.includes("\n")) {
-          const lines = child.textContent.split("\n");
+        if (child.textContent?.includes('\n')) {
+          const lines = child.textContent.split('\n');
           const fragment = document.createDocumentFragment();
           lines.forEach((line) => {
-            const lineSpan = document.createElement("span");
+            const lineSpan = document.createElement('span');
             lineSpan.textContent = line;
             fragment.appendChild(lineSpan);
           });
           child.parentElement?.replaceChild(fragment, child);
         } else {
-          child.innerHTML = child.textContent || "";
+          child.innerHTML = child.textContent || '';
         }
       }
     });
@@ -53,22 +59,22 @@ function adjustDiffCodeBlock(dom: HTMLElement) {
 }
 
 function removeMessageSymbol(dom: HTMLElement) {
-  const messageSymbols = dom.querySelectorAll(".msg-symbol");
+  const messageSymbols = dom.querySelectorAll('.msg-symbol');
   messageSymbols.forEach((el) => {
     el.remove();
   });
 }
 
 function addCodeBlockFileName(dom: HTMLElement) {
-  const codeBlockContainers = dom.querySelectorAll(".code-block-container");
+  const codeBlockContainers = dom.querySelectorAll('.code-block-container');
   codeBlockContainers.forEach((codeBlockContainer) => {
     if (codeBlockContainer.children.length === 1) {
       // ファイル名がないので追加する
-      const fileNameDom = document.createElement("div");
-      fileNameDom.className = "code-block-filename-container";
+      const fileNameDom = document.createElement('div');
+      fileNameDom.className = 'code-block-filename-container';
       codeBlockContainer.insertBefore(
         fileNameDom,
-        codeBlockContainer.firstChild,
+        codeBlockContainer.firstChild
       );
     }
   });
@@ -76,19 +82,19 @@ function addCodeBlockFileName(dom: HTMLElement) {
 
 function removeCodeBlockEndNewLine(dom: HTMLElement) {
   // 原因は不明だが、マークダウンペーストだと通常なら文末、差分なら行末に余分な改行が1つ増える
-  const codeBlocks = dom.querySelectorAll("pre code:not(.diff-highlight)");
+  const codeBlocks = dom.querySelectorAll('pre code:not(.diff-highlight)');
   codeBlocks.forEach((codeBlock) => {
-    const code = codeBlock.textContent || "";
-    if (code.endsWith("\n")) {
+    const code = codeBlock.textContent || '';
+    if (code.endsWith('\n')) {
       codeBlock.textContent = code.slice(0, -1);
     }
   });
 
-  const diffCodeBlocks = dom.querySelectorAll("pre code.diff-highlight");
+  const diffCodeBlocks = dom.querySelectorAll('pre code.diff-highlight');
   diffCodeBlocks.forEach((codeBlock) => {
     codeBlock.childNodes.forEach((child) => {
-      const code = child.textContent || "";
-      if (code.endsWith("\n")) {
+      const code = child.textContent || '';
+      if (code.endsWith('\n')) {
         child.textContent = code.slice(0, -1);
       }
     });
@@ -98,7 +104,7 @@ function removeCodeBlockEndNewLine(dom: HTMLElement) {
 // URL単体の埋め込み要素は、不要なリンクとパラグラフを持つので削除する
 function removeEmbedDeco(dom: HTMLElement) {
   const embeds = dom.querySelectorAll(
-    ".zenn-embedded-github, .zenn-embedded-tweet, .zenn-embedded-card, .embed-youtube",
+    '.zenn-embedded-github, .zenn-embedded-tweet, .zenn-embedded-card, .embed-youtube'
   );
   embeds.forEach((embed) => {
     // 不要なリンクを削除
@@ -106,9 +112,9 @@ function removeEmbedDeco(dom: HTMLElement) {
 
     // 不要な親のpタグを削除し、埋め込み要素を親要素の位置に置換
     const notUsedP = embed.parentElement;
-    if (notUsedP?.tagName !== "P") {
+    if (notUsedP?.tagName !== 'P') {
       console.error(embed);
-      throw new Error("should be embed with only url");
+      throw new Error('should be embed with only url');
     }
 
     notUsedP.parentElement?.replaceChild(embed, notUsedP);
@@ -116,16 +122,16 @@ function removeEmbedDeco(dom: HTMLElement) {
 }
 
 function adjustFootnotes(dom: HTMLElement) {
-  const footnotes = dom.querySelector("section.footnotes");
+  const footnotes = dom.querySelector('section.footnotes');
   footnotes?.firstChild?.remove(); // titleを削除
 
-  const items = footnotes?.querySelectorAll("li");
+  const items = footnotes?.querySelectorAll('li');
   items?.forEach((item) => {
-    const backRefAnchor = item.querySelector("a.footnote-backref");
-    const id = backRefAnchor?.getAttribute("href")?.replace("#", "");
-    if (!id) throw new Error("footnote back reference id not found");
+    const backRefAnchor = item.querySelector('a.footnote-backref');
+    const id = backRefAnchor?.getAttribute('href')?.replace('#', '');
+    if (!id) throw new Error('footnote back reference id not found');
 
-    item.setAttribute("data-footnote-reference-id", id);
+    item.setAttribute('data-footnote-reference-id', id);
     backRefAnchor?.remove();
   });
 }
