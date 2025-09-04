d947ac84c26ba8cd6e578762c834be96b857c92e
karintou8710 <karintou8710@gmail.com>
2025-09-05 01:48:17 +0900

    fix: マークダウン出力で言語なしを出力しない

--- packages/zenn-wysiwyg-editor/src/lib/to-markdown.ts
+++ packages/zenn-wysiwyg-editor/src/lib/to-markdown.ts
@@ -85,8 +85,8 @@ const markdownSerializer = new MarkdownSerializer(
       const backticks = preContentNode.textContent.match(/`{3,}/gm);
       const fence = backticks ? `${backticks.sort().slice(-1)[0]}\`` : '```';
       const isDiff = preContentNode.attrs.language?.startsWith('diff');
-      const language =
-        preContentNode.attrs.language?.replace(/diff-?/, '') || 'plaintext';
+      let language = preContentNode.attrs.language?.replace(/diff-?/, '') || '';
+      language = language.replace(/^(plain|plaintext)$/, ''); // plain, plaintextは言語指定なしとみなす
 
       state.write(
         fence +
