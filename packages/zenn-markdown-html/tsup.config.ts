f46dd5fc75d7b2222ab2b63648c06974c0c6f712
karintou8710 <karintou8710@gmail.com>
2025-09-05 19:45:25 +0900

    fix: コードシンタックスをAllに

--- packages/zenn-markdown-html/tsup.config.ts
+++ packages/zenn-markdown-html/tsup.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'tsup';
+import { prismjsPlugin } from 'esbuild-plugin-prismjs';
 
 export default defineConfig({
   entry: ['src/index.ts'],
@@ -8,4 +9,5 @@ export default defineConfig({
   clean: true,
   sourcemap: true,
   format: ['esm'],
+  esbuildPlugins: [prismjsPlugin({ languages: 'all' })],
 });
