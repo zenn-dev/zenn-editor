be3123f2f4c4fba1e68c327bfc836c7e132415c4
karintou8710 <karintou8710@gmail.com>
2025-09-07 21:10:06 +0900

    chore: 生成ファイルを直接実行可能に

--- packages/zenn-cli/tsup.config.ts
+++ packages/zenn-cli/tsup.config.ts
@@ -9,4 +9,20 @@ export default defineConfig({
   sourcemap: true,
   format: ['cjs'],
   env: dotenv.config().parsed,
+  esbuildPlugins: [
+    {
+      name: 'add-banner',
+      setup(build) {
+        build.onEnd((result) => {
+          const file = result.outputFiles?.find((f) =>
+            f.path.endsWith('zenn.js')
+          );
+          if (!file) throw new Error('zenn.js not exists');
+
+          const text = `#!/usr/bin/env node\n${file.text}`;
+          file.contents = Buffer.from(text);
+        });
+      },
+    },
+  ],
 });
