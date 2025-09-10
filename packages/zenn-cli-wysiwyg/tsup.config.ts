3513a6aa9ba08459496621658e575cb53bffff6e
karintou8710 <karintou8710@gmail.com>
2025-09-10 15:00:43 +0900

    chore: モノレポの依存関係を調整

--- packages/zenn-cli-wysiwyg/tsup.config.ts
+++ packages/zenn-cli-wysiwyg/tsup.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'tsup';
 import dotenv from 'dotenv';
 
+// zenn-* は devDependencies にしてバンドルに含める (zenn-wysiwyg-editor を除く)
 export default defineConfig({
   entry: ['./src/server/zenn.ts'],
   tsconfig: './tsconfig.server.json',
@@ -9,26 +10,7 @@ export default defineConfig({
   sourcemap: true,
   format: ['cjs'],
   env: dotenv.config().parsed,
-  noExternal: [
-    'zenn-markdown-html',
-    'zenn-content-css',
-    'zenn-model',
-    'zenn-embed-elements',
-  ],
-  esbuildPlugins: [
-    {
-      name: 'add-banner',
-      setup(build) {
-        build.onEnd((result) => {
-          const file = result.outputFiles?.find((f) =>
-            f.path.endsWith('zenn.js')
-          );
-          if (!file) throw new Error('zenn.js not exists');
-
-          const text = `#!/usr/bin/env node\n${file.text}`;
-          file.contents = Buffer.from(text);
-        });
-      },
-    },
-  ],
+  banner: {
+    js: '#!/usr/bin/env node\n',
+  },
 });
