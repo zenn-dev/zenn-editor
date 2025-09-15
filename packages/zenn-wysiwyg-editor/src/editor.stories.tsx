610039d2b86c53c63831ca2e598f2a2fa7408ce6
karintou8710 <karintou8710@gmail.com>
2025-09-15 23:24:28 +0900

    chore: storyの種類を追加

--- packages/zenn-wysiwyg-editor/src/editor.stories.tsx
+++ packages/zenn-wysiwyg-editor/src/editor.stories.tsx
@@ -2,25 +2,19 @@ import type { Meta, StoryObj } from '@storybook/react-vite';
 import { useZennEditor } from '.';
 import EditorContent from './components/editor/editor-content';
 
-function Editor() {
+type EditorProps = {
+  initialContent?: string;
+};
+
+function Editor({ initialContent }: EditorProps) {
   const editor = useZennEditor({
-    initialContent: '',
+    initialContent: initialContent || '',
   });
   return <EditorContent editor={editor} />;
 }
 
 const meta = {
   component: Editor,
-} satisfies Meta<typeof Editor>;
-
-export default meta;
-type Story = StoryObj<typeof meta>;
-
-export const Basic: Story = {
-  args: {
-    primary: true,
-    label: 'Editor',
-  },
   parameters: {
     layout: 'centered',
   },
@@ -31,4 +25,33 @@ export const Basic: Story = {
       </div>
     ),
   ],
+} satisfies Meta<typeof Editor>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Empty: Story = {};
+export const Heading: Story = {
+  args: {
+    initialContent:
+      '<h1>Heading 1</h1><h2>Heading 2</h2><h3>Heading 3</h3><h4>Heading 4</h4>',
+  },
+};
+
+export const Blockquote: Story = {
+  args: {
+    initialContent:
+      '<blockquote><p>Blockquote content</p><p>Blockquote content</p></blockquote>',
+  },
+};
+
+export const CodeBlock: Story = {
+  args: {
+    initialContent: `
+    <div class="code-block-container"><div class="code-block-filename-container"><span class="code-block-filename"></span></div>
+    <pre><code class="language-javascript">console.log("hello");</code></pre></div>
+    <div class="code-block-container"><div class="code-block-filename-container"><span class="code-block-filename">example.ts</span></div>
+    <pre><code class="language-diff-python diff-highlight"><span>+ import os</span></code></pre></div>
+    `,
+  },
 };
