e4a75279a52a6521d448becfadb0de244e16ec4a
karintou8710 <karintou8710@gmail.com>
2025-09-21 18:40:20 +0900

    feat: コンテンツをlocalStorageに保存

--- packages/zenn-web/src/app.tsx
+++ packages/zenn-web/src/app.tsx
@@ -6,14 +6,21 @@ import type { TocNode } from 'zenn-model/lib/types';
 import { EditorContent, useZennEditor } from 'zenn-wysiwyg-editor';
 
 import { Toc } from './components/toc';
+import { usePersistedState } from './hooks/use-persisted-state';
 import styles from './app.module.css';
 
 function App() {
   const [toc, setToc] = useState<TocNode[]>([]);
+  const [persistedContent, setPersistedContent] = usePersistedState<string>({
+    cacheKey: 'zenn-editor-content',
+    defaultValue: '',
+  });
+
   const editor = useZennEditor({
-    initialContent: '',
+    initialContent: persistedContent,
     onChange: (html) => {
       setToc(parseToc(html));
+      setPersistedContent(html);
     },
   });
 
